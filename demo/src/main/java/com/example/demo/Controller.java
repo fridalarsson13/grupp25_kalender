90aff8ca7e06b41a5a1efb61844b6e4e72ff2acf
fridalarsson13 <fridalarsson25@gmail.com>
2025-04-01 10:44:19 +0200

    kalender layout basic

--- demo/src/main/java/com/example/demo/Controller.java
+++ demo/src/main/java/com/example/demo/Controller.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
+    @Value("${spring.application.name}")
+    private String name;
     @RequestMapping("/")
     public String index() {
-        return "index.html";
+        System.out.println(name);
+        return "button.html";
     }
 }
